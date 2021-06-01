6d70c6d09582647070ca55ebb3eeb36739a6df2d
Shweta tripathi <77456530+shweta-tripathe@users.noreply.github.com>
2021-06-02 00:57:49 +0530

    Add files via upload

---
+++ Ip-tracker/app.js
@@ -0,0 +1,29 @@
+const lookupBtn = document.querySelector(".lookup-btn");
+const ipDisplay = document.querySelector(".ip-display");
+const locationDisplay = document.querySelector(".location-display");
+const geoDisplay = document.querySelector(".geo-display");
+const loader = document.querySelector(".loader-container");
+const details = document.querySelector(".details");
+
+
+lookupBtn.addEventListener("click", () => {
+    loader.style.display = "flex";
+    details.style.display = "none"
+
+    axios.get("https://ipapi.co/json/").then((response) => {
+        loader.style.display = "none";
+        details.style.display = "flex";
+
+        ipDisplay.textContent = `Ip: ${response.data.ip}`;
+
+locationDisplay.textContent = `Location: ${response.data.city}, ${response.data.region}, ${response.data.country_name}`;
+
+geoDisplay.textContent =`Geo Location: ${response.data.latitude}, ${response.data.longitude}`;
+    });
+
+
+
+
+});
+
+
